91ef5dc0b5782e6770d4864ca3d27ce66ca03f8f
Emesson-cmd <emerssosnkavallcante@gmail.com>
2023-04-22 15:16:52 -0300

    chore: adding notifications

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import './src/lib/dayjs';
 
-import { StatusBar } from 'react-native';
+import { StatusBar, Button } from 'react-native';
 import {
   useFonts,
   Inter_400Regular,
@@ -8,9 +8,18 @@ import {
   Inter_700Bold,
   Inter_800ExtraBold,
 } from '@expo-google-fonts/inter';
+import * as Notifications from 'expo-notifications';
 
 import { Loading } from './src/components/Loading';
 import { Routes } from './src/routes';
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+  }),
+});
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -20,14 +29,34 @@ export default function App() {
     Inter_800ExtraBold,
   });
 
+  async function scheduleNotification() {
+    const trigger = new Date(Date.now());
+    trigger.setMinutes(trigger.getMinutes() + 1);
+
+    await Notifications.scheduleNotificationAsync({
+      content: {
+        title: 'Olá, Rodrigo, 👋🤏',
+        body: 'Você praticou seus hábitos hoje?',
+      },
+      trigger,
+    });
+  }
+
+  async function getScheduleNotification() {
+    const schedules = await Notifications.getAllScheduledNotificationsAsync();
+    console.log(schedules)
+  }
+  
   if (!fontsLoaded) {
     return <Loading />;
   }
 
   return (
     <>
+      <Button title="Enviar Notificação" onPress={scheduleNotification} />
+      <Button title="Enviar" onPress={getScheduleNotification} />
       <Routes />
-      <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
+      <StatusBar barStyle="light-content" backgroundColor="transparent" /* translucent */ />
     </>
   );
 }
